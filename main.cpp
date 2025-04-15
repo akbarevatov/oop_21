a660d4bb515199a7d0076a1ec9b8e12962fe5b49
Shahrizodaa1120 <s.joraqulova@newuu.uz>
2025-04-15 13:47:09 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Menu.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Create and run the Menu system.
+    Menu menu;
+    menu.displayMenu();
+    return 0;
+}
+
+
+
+
