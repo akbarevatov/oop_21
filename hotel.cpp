84429da8554720fe63bf90739f00e604e1a5e5aa
mavludaSotvoldiyeva <m.sotvoldiyeva@newuu.uz>
2025-04-16 16:12:49 +0500

    Update hotel.cpp

--- hotel.cpp
+++ hotel.cpp
@@ -1,4 +1,5 @@
 #include "hotel.h"
+#include <ctime>
 #include <iostream>
 using namespace std;
 Hotel::Hotel() {}
@@ -13,8 +14,11 @@ void Hotel::addEmployee(Employee* e) {
     employees.push_back(e);
 }
 void Hotel::createBooking(Guest* g, Room& r) {
+    string bookingID = "BOOK" + to_string(time(0));
+    string startDate = "2025-04-14";
+    string endDate = "2025-04-15";
     cout << "Creating booking for guest and room: " << r.getRoomNumber() << endl;
-    Booking* newBooking = new Booking(g, &r);
+    Booking* newBooking = new Booking(bookingID, g, &r, startDate, endDate);
     bookings.push_back(newBooking);
 }
 void Hotel::generateReport() {
@@ -25,4 +29,21 @@ void Hotel::generateReport() {
 }
 vector<Room>& Hotel::getRooms() {
     return rooms;
+}
+vector<Booking*>& Hotel::getBookings() {
+    return bookings;
+}
+vector<Guest*>& Hotel::getGuests() {
+    return guests;
+}
+vector<Feedback*>& Hotel::getFeedbacks() {
+    return feedbacks;
+}
+void Hotel::addFeedback(Feedback* feedback) {
+    feedbacks.push_back(feedback);
+}
+void Hotel::createBooking(Guest* g, Room& r, string startDate, string endDate) {
+    string bookingID = to_string(bookings.size() + 1);
+    Booking* booking = new Booking(bookingID, g, &r, startDate, endDate);
+    bookings.push_back(booking);
 }
